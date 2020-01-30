a621ee5a9b646ce938c22584cc9cecc987f0c2c9
Antwan Maddox <antwan.maddox@agilethought.com>
2020-01-30 12:48:52 -0500

    initial commit

---
+++ StatsRoyaleTests/CardsTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace StatsRoyaleTests
+{
+    [TestClass]
+    public class CardsTest
+    {
+        public IWebDriver _driver = null;
+
+        [TestMethod]
+        public void Assert_IceSpiritCard_IsOnCardPage()
+        {
+            //Navigate to Cards Page
+            var cardMenu = _driver.FindElement(By.CssSelector("a[href='/cards']"));
+            cardMenu.Click();
+
+            var card = _driver.FindElement(By.CssSelector("a[href='https://statsroyale.com/card/Ice+Spirit']"));
+            //Verify Ice Spirit Card Shows Up
+
+            Assert.IsTrue(card.Displayed,"The Ice Spirit Card is not displayed!");
+        }
+        [TestMethod]
+        public void Assert_IceSpiritCardDetailStats_AreCorrect()
+        {
+            //Navigate to Cards Page
+            var cardMenu = _driver.FindElement(By.CssSelector("a[href='/cards']"));
+            cardMenu.Click();
+            var card = _driver.FindElement(By.CssSelector("a[href='https://statsroyale.com/card/Ice+Spirit']"));
+
+            card.Click();
+            
+            var cardRarityText = _driver.FindElement(By.CssSelector("div[class='card__rarity']")).Text.Split(',');
+
+            Assert.AreEqual(cardRarityText[0], "Troop");
+            Assert.AreEqual(cardRarityText[1], " Arena 8");
+
+
+        }
+
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("https://statsroyale.com/");
+            _driver.Manage().Window.Maximize();
+           
+
+        }
+        [TestCleanup]
+        public void Teardown()
+        {
+            _driver.Quit();
+        }
+    }
+}
---
+++ StatsRoyaleTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StatsRoyaleTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StatsRoyaleTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ea892230-4b42-42ce-9429-0e88c1d96a8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
